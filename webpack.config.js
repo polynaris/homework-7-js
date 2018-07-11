851672912a380d76d2520a2d16ce36776251eec5
Polina Tumanian <>
2018-07-11 11:16:05 +0300

    removed redundant folder

--- webpack.config.js
+++ webpack.config.js
@@ -2,39 +2,45 @@ const path = require('path');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
-  entry: {
-    main: path.resolve(__dirname, 'src', 'index.jsx')
-  },
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.jsx?$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader'
+    entry: {
+        main: path.resolve(__dirname, 'src', 'index.jsx')
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [{
+            test: /\.jsx?$/,
+            exclude: /node_modules/,
+            use: {
+                loader: 'babel-loader',
+                options: {
+                    presets: ['react']
+                }
+            }
+        },
+        {
+            test: /\.css$/,
+            use: ExtractTextPlugin.extract({
+                fallback: 'style-loader',
+                use: ['css-loader']
+            })
         }
-      },
-      {
-        test: /\.css$/,
-        use: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: ['css-loader']
+        ]
+    },
+    plugins: [
+        new ExtractTextPlugin({ filename: 'style.css' }),
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, 'src', 'index.html'),
+            filename: 'index.html'
         })
-      }
-    ]
-  },
-  plugins: [
-    new ExtractTextPlugin({ filename: 'style.css' }),
-    new HtmlWebpackPlugin({ 
-      template: path.resolve(__dirname, 'src', 'index.html'),
-      filename: 'index.html'
-    })
-  ],
-  devServer: {
-    historyApiFallback: true
-  }
+    ],
+    devServer: {
+        historyApiFallback: true,
+        proxy: [{
+            path: '/pokemons/',
+            target: 'http://localhost:3000'
+        }]
+    }
 };
